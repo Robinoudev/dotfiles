ea2008371b429867e24372a9369db9bb184c62f9
Robin Oude Vrielink <robin@oudevrielink.net>
2021-06-30 20:45:32 +0200

    bind ctrl+R to search in zsh hist

--- config/zsh/keybinds.zsh
+++ config/zsh/keybinds.zsh
@@ -3,6 +3,9 @@ bindkey -v
 bindkey -M viins 'jk' vi-cmd-mode
 bindkey -M viins ' ' magic-space
 
+# history search
+bindkey '^R' history-incremental-search-backward
+
 # Shift + Tab
 bindkey -M viins '^[[Z' reverse-menu-complete
 # bind UP and DOWN arrow keys
