b692a48c09f6e81d486f59eae8ecc9e3009a27f3
Robin Oude Vrielink <robin@oudevrielink.net>
2021-11-16 15:25:57 +0100

    feat: added chromium module

--- hosts/katsumi/default.nix
+++ hosts/katsumi/default.nix
@@ -24,6 +24,7 @@
         firefox.enable = true;
         # qutebrowser.enable = true;
         brave.enable = true;
+        chromium.enable = true;
       };
       steam.enable = true;
     };
---
+++ modules/desktop/browsers/chromium.nix
@@ -0,0 +1,24 @@
+{ options, config, lib, pkgs, ... }:
+
+with lib;
+with lib.my;
+let cfg = config.modules.desktop.browsers.chromium;
+in {
+  options.modules.desktop.browsers.chromium = with types; {
+    enable = mkBoolOpt false;
+  };
+
+  config = mkIf cfg.enable {
+    programs.chromium = {
+      enable = true;
+      extensions = [
+        "dpjamkmjmigaoobjbekmfgabipmfilij" # Empty new tab page
+        "bkdgflcldnnnapblkhphbgpggdiikppg" # DuckDuckGo Privacy Essentials
+        "nngceckbapebfimnlniiiahkandclblb" # Bitwarden
+        "pkehgijcmpdhfbdbbnkijodmdjhbjlgp" # Privacy Badger
+        "njdfdhgcmkocbgbhcioffdbicglldapd" # LocalCDN
+        "ckkdlimhmcjmikdlpkmbgfkaikojcbjk" # Markdown Viewer
+      ];
+    };
+  };
+}
